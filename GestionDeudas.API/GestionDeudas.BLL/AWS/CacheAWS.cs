74063ba37df839715065e24ca9e98acc694d533b
DanielLinares <dlinares16@yahoo.es>
2025-09-05 19:24:47 -0500

    Primeros avances en configurar cache de aws (Errores)

---
+++ GestionDeudas.API/GestionDeudas.BLL/AWS/CacheAWS.cs
@@ -0,0 +1,95 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using GestionDeudas.BLL.AWS.Contrato;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GestionDeudas.BLL.AWS
+{
+    public class CacheAWS : ICacheAWS
+    {
+        private readonly IAmazonDynamoDB _dynamoDb;
+        private readonly string _tableName;
+
+        public CacheAWS(
+            IAmazonDynamoDB dynamoDb,
+            IConfiguration configuration)
+        {
+            _dynamoDb = dynamoDb;
+            _tableName = configuration.GetValue<string>("DynamoDb:CacheTableName") ?? "DebtCache";
+        }
+
+        public Task DeleteCacheAsync(string cacheKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsCacheAsync(string cacheKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T?> GetCacheAsync<T>(string cacheKey) where T : class
+        {
+            try
+            {
+                var request = new GetItemRequest
+                {
+                    TableName = _tableName,
+                    Key = new Dictionary<string, AttributeValue>
+                    {
+                        { "CacheKey", new AttributeValue { S = cacheKey } }
+                    }
+                };
+
+                var response = await _dynamoDb.GetItemAsync(request);
+
+                // Si no se encontró el item
+                if (!response.Item.Any())
+                {
+                    return null;
+                }
+
+                // Verificar si los datos han expirado
+                if (response.Item.ContainsKey("ExpiresAt"))
+                {
+                    if (DateTime.TryParse(response.Item["ExpiresAt"].S, out var expiresAt))
+                    {
+                        if (DateTime.UtcNow > expiresAt)
+                        {
+                            // Eliminar datos expirados
+                            _ = Task.Run(() => DeleteAsync(cacheKey));
+                            return null;
+                        }
+                    }
+                }
+
+                // Tenemos datos válidos
+                if (!response.Item.ContainsKey("Data"))
+                {
+                    return null;
+                }
+
+                var jsonData = response.Item["Data"].S;
+                var deserializedData = JsonSerializer.Deserialize<T>(jsonData);
+
+                return deserializedData;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public Task SetCacheAsync<T>(string cacheKey, T data, TimeSpan? expiration = null) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GestionDeudas.API/GestionDeudas.BLL/AWS/Contrato/ICacheAWS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeudas.BLL.AWS.Contrato
+{
+    public interface ICacheAWS
+    {
+        Task<T?> GetCacheAsync<T>(string cacheKey) where T : class;
+        Task SetCacheAsync<T>(string cacheKey, T data, TimeSpan? expiration = null) where T : class;
+        Task DeleteCacheAsync(string cacheKey);
+        Task<bool> ExistsCacheAsync(string cacheKey);
+
+    }
+}
